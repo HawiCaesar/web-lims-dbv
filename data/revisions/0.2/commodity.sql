7af6c0057e7ee55e69749ab4d943b352e057d750
HawiCaesar <brian.odhiambo932@gmail.com>
2015-02-03 12:21:52 +0300

    Removed some commodities and their categories

--- data/revisions/0.2/commodity.sql
+++ data/revisions/0.2/commodity.sql
@@ -3,16 +3,15 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 29, 2015 at 01:34 PM
+-- Generation Time: Feb 03, 2015 at 10:15 AM
 -- Server version: 5.6.16
 -- PHP Version: 5.5.11
 
--- Author Brian Hawi
--- first version of commodity 0.2
--- has a status field
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
+
+-- Author Brian Hawi
+-- Removed Haematology and Biochemistry Commodities
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -53,32 +52,7 @@ INSERT INTO `commodity` (`id`, `code`, `name`, `unit`, `category_id`, `status`) 
 (6, 'FAC 002', 'FACSCount CD4 % reagent [Paediatric]', 'Tests', 3, 1),
 (7, '', 'EASY Count CD4/CD3 Reagent [Adult]', 'Tests', 2, 1),
 (8, '', 'EASY Count CD4 % reagent [Paediatric]', 'Tests', 2, 1),
-(9, '', 'Cartridges', 'Pieces', 4, 1),
-(10, '', 'Cell / ACT Pack', 'Litres', 5, 1),
-(11, '', 'Cell Clean / ACT Rinse', 'Litres', 5, 1),
-(12, '', 'ACT Diff 3 kits', 'Tests', 5, 1),
-(13, '', 'ACT Diff 5 kits', 'Tests', 5, 1),
-(14, '', 'Celtac 6400 kits', 'Tests', 5, 1),
-(15, '', 'Celtac 8222', 'Tests', 5, 1),
-(16, '', 'Haemo cuvettes', 'Tests', 5, 1),
-(17, '', 'Albumin', 'Tests', 6, 1),
-(18, '', 'Alkaline phosphatase', 'Tests', 6, 1),
-(19, '', 'ALT (SGPT)', 'Tests', 6, 1),
-(20, '', 'AST (SGOT)', 'Tests', 6, 1),
-(21, '', 'Creatinine', 'Tests', 6, 1),
-(22, '', 'Potassium', 'Tests', 6, 1),
-(23, '', 'Sodium', 'Tests', 6, 1),
-(24, '', 'Chloride', 'Tests', 6, 1),
-(25, '', 'Gamma GT', 'Tests', 6, 1),
-(26, '', 'Glucose test strips', 'Pieces', 6, 1),
-(27, '', 'HDL Cholesterol', 'Tests', 6, 1),
-(28, '', 'Pregnancy test strips', 'Pieces', 6, 1),
-(29, '', 'Serum Amylase test kits', 'Tests', 6, 1),
-(30, '', 'Serum protein kit', 'Tests', 6, 1),
-(31, '', 'Total Cholesterol', 'Tests', 6, 1),
-(32, '', 'Triglycerides', 'Tests', 6, 1),
-(33, '', 'Bilirubin', 'Tests', 6, 1),
-(34, '', 'Urea', 'Tests', 6, 1);
+(9, '', 'Cartridges', 'Pieces', 4, 1);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
--- data/revisions/0.2/commodity_category.sql
+++ data/revisions/0.2/commodity_category.sql
@@ -3,16 +3,15 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 29, 2015 at 01:33 PM
+-- Generation Time: Feb 03, 2015 at 10:15 AM
 -- Server version: 5.6.16
 -- PHP Version: 5.5.11
 
--- Author Brian Hawi
--- first version of commodity_category
--- the equipment_id column is set to zero as there is are no equipment yet
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
+
+-- Author Brian Hawi
+-- Removed Haematology and Biochemistry Commodity Categories
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -35,7 +34,7 @@ CREATE TABLE IF NOT EXISTS `commodity_category` (
   `name` varchar(100) NOT NULL,
   `equipment_id` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
 
 --
 -- Dumping data for table `commodity_category`
@@ -45,9 +44,7 @@ INSERT INTO `commodity_category` (`id`, `name`, `equipment_id`) VALUES
 (1, 'FACS Calibur Reagents and Consumables', 0),
 (2, 'Cyflow Partec Reagents', 0),
 (3, 'FACS Count Reagents', 0),
-(4, 'Point of Care CD4 reagents (e.g. PIMA, etc)', 0),
-(5, 'Haematology', 0),
-(6, 'Biochemistry', 0);
+(4, 'Point of Care CD4 reagents (e.g. PIMA, etc)', 0);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
